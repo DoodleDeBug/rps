df85361b0260bf2e447cf30897e30a32d6771cca
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-04-26 19:53:50 +0100

    moved code around

--- rps.js
+++ rps.js
@@ -7,63 +7,62 @@ const winMessage = "You Won!";
 const loseMessage = "You Lost!";
 const tieMessage = "It's A Tie!";
 
-let roundNo = document.querySelector("#roundNo");
+let roundNo = (document.querySelector("#roundNo").textContent = 1);
 const msg = document.querySelector(".msg");
-let score = document.querySelector(".score");
+
 const container = document.querySelector(".container");
 
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
-function game(playerSelection) {
-  console.log(playerSelection);
-  // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
-  function computerPlay() {
-    let choices = ["Rock", "Paper", "Scissors"];
-    let randomNumber = Math.floor(Math.random() * choices.length);
-    return choices[randomNumber];
-  }
+// randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+function computerPlay() {
+  let choices = ["Rock", "Paper", "Scissors"];
+  let randomNumber = Math.floor(Math.random() * choices.length);
+  return choices[randomNumber];
+}
 
-  computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
+// a single round of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+  let win =
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper");
+  let lose =
+    (playerSelection === "Paper" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock") ||
+    (playerSelection === "Rock" && computerSelection === "Paper");
 
-  // a single round of rock paper scissors
-  function playRound(playerSelection, computerSelection) {
-    let win =
-      (playerSelection === "Rock" && computerSelection === "Scissors") ||
-      (playerSelection === "Paper" && computerSelection === "Rock") ||
-      (playerSelection === "Scissors" && computerSelection === "Paper");
-    let lose =
-      (playerSelection === "Paper" && computerSelection === "Scissors") ||
-      (playerSelection === "Scissors" && computerSelection === "Rock") ||
-      (playerSelection === "Rock" && computerSelection === "Paper");
-
-    if (win) {
-      playerScore += 1;
-      console.log("Player Score: " + playerScore);
-      console.log("Computer Score: " + computerScore);
-      console.log(
-        `${playerSelection} beats ${computerSelection}! ${winMessage}`
-      );
-      return;
-    } else if (lose) {
-      computerScore += 1;
-      console.log("Player Score: " + playerScore);
-      console.log("Computer Score: " + computerScore);
-      console.log(
-        `${computerSelection} beats ${playerSelection}! ${loseMessage}`
-      );
-      return;
-    } else if (playerSelection === computerSelection) {
-      console.log("Player Score: " + playerScore);
-      console.log("Computer Score: " + computerScore);
-      console.log(tieMessage);
-      return;
-    }
+  if (win) {
+    playerScore += 1;
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+    console.log(`${playerSelection} beats ${computerSelection}! ${winMessage}`);
+    return;
+  } else if (lose) {
+    computerScore += 1;
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+    console.log(
+      `${computerSelection} beats ${playerSelection}! ${loseMessage}`
+    );
+    return;
+  } else if (playerSelection === computerSelection) {
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+    console.log(tieMessage);
+    return;
   }
 }
 
 rock.addEventListener("click", this.game.bind(this, "Rock"));
 paper.addEventListener("click", this.game.bind(this, "Paper"));
-scissors.addEventListener("click", this.game.bind(this, "Rock"));
+scissors.addEventListener("click", this.game.bind(this, "Scissors"));
+
+// play the game
+function game(playerSelection) {
+  console.log(playerSelection);
+  computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+}
